6924d8bb395b7858dd54fb40736a586bea561582
Omkar Gade <121718985+omkargade04@users.noreply.github.com>
2026-02-01 23:16:35 +0530

    Fix import statement for User in post.controller.ts

--- server/controllers/post.controller.ts
+++ server/controllers/post.controller.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { ReqMid } from "../types/user"; 
+import { ReqMid } from "../types/user";
+import User from "./db/user";
 
 const prisma = new PrismaClient();
 
